1f88244e868881099fb62c280e465292daf64f6a
Peter <peter@Peterde-MacBook-Pro.local>
2020-12-27 18:05:56 +0800

    Update time.go

--- time.go
+++ time.go
@@ -3,6 +3,7 @@ package timeby77
 import (
 	"math"
 	"strconv"
+	"time"
 )
 
 func HowLongAgo(timebefore int64, timenow int64) (strres string) {
@@ -30,4 +31,63 @@ func HowLongAgo(timebefore int64, timenow int64) (strres string) {
 		break
 	}
 	return strres
+}
+
+func GetAge(BirthDay time.Time, Now time.Time) (age int) {
+	age = 0
+	age = Now.Year() - BirthDay.Year()
+	if age < 0 {
+		age = 0
+	}
+	return age
+}
+
+func GetConstellation(BirthDay time.Time) (star string) {
+
+	month := BirthDay.Month()
+	day := BirthDay.Day()
+	star = "未知"
+	if month <= 0 || month >= 13 {
+		star = "未知"
+	}
+	if day <= 0 || day >= 32 {
+		star = "未知"
+	}
+	if (month == 1 && day >= 20) || (month == 2 && day <= 18) {
+		star = "水瓶座"
+	}
+	if (month == 2 && day >= 19) || (month == 3 && day <= 20) {
+		star = "雙魚座"
+	}
+	if (month == 3 && day >= 21) || (month == 4 && day <= 19) {
+		star = "白羊座"
+	}
+	if (month == 4 && day >= 20) || (month == 5 && day <= 20) {
+		star = "金牛座"
+	}
+	if (month == 5 && day >= 21) || (month == 6 && day <= 21) {
+		star = "雙子座"
+	}
+	if (month == 6 && day >= 22) || (month == 7 && day <= 22) {
+		star = "巨蟹座"
+	}
+	if (month == 7 && day >= 23) || (month == 8 && day <= 22) {
+		star = "狮子座"
+	}
+	if (month == 8 && day >= 23) || (month == 9 && day <= 22) {
+		star = "處女座"
+	}
+	if (month == 9 && day >= 23) || (month == 10 && day <= 22) {
+		star = "天枰座"
+	}
+	if (month == 10 && day >= 23) || (month == 11 && day <= 21) {
+		star = "天蠍座"
+	}
+	if (month == 11 && day >= 22) || (month == 12 && day <= 21) {
+		star = "射手座"
+	}
+	if (month == 12 && day >= 22) || (month == 1 && day <= 19) {
+		star = "摩羯座"
+	}
+	return star
 }
