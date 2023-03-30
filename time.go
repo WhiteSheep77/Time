cc930610295cdab8db87ae3f26a87f1aa9ffd70e
WhiteSheep77 <peterpeteraa7803@gmail.com>
2023-03-31 04:24:01 +0800

    Update time.go

--- time.go
+++ time.go
@@ -33,6 +33,33 @@ func HowLongAgo(timebefore int64, timenow int64) (strres string) {
 	return strres
 }
 
+func HowLongAgoJp(timebefore int64, timenow int64) (strres string) {
+	var TimeUint = [6]int64{365 * 86400, 30 * 86400, 86400, 3600, 60, 1}
+	var StrUnit = [6]string{"年前", "ヶ月前", "日前", "時間前", "分前", "秒前"}
+	strres = "たった今"
+
+	if timebefore > timenow {
+		return
+	}
+
+	diff := timenow - timebefore
+
+	for i := 0; i < len(TimeUint); i++ {
+		if diff < TimeUint[i] {
+			continue
+		}
+		var temp = math.Floor(float64(diff / TimeUint[i]))
+		diff = diff % TimeUint[i]
+		if temp > 0 {
+			var tempStr string
+			tempStr = strconv.FormatFloat(temp, 'f', -1, 64)
+			strres = tempStr + StrUnit[i]
+		}
+		break
+	}
+	return strres
+}
+
 func Time2DateType1(Input time.Time) (OutPut string) {
 
 	OutPut = Input.Format("01/02 15:04")
