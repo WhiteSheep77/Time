6b3598f0db8d751b02a5efb3a66d805bc09dc9a4
WhiteSheep77 <peterpeteraa7803@gmail.com>
2023-05-14 13:52:14 +0800

    Update time.go

--- time.go
+++ time.go
@@ -33,6 +33,37 @@ func HowLongAgo(timebefore int64, timenow int64) (strres string) {
 	return strres
 }
 
+func HowLongAgoMaxMonth(timebefore int64, timenow int64) (strres string) {
+	var TimeUint = [6]int64{365 * 86400, 30 * 86400, 86400, 3600, 60, 1}
+	var StrUnit = [6]string{"年前", "月前", "天前", "小時前", "分鐘前", "秒鐘前"}
+	strres = "剛剛"
+
+	if timebefore > timenow {
+		return
+	}
+
+	diff := timenow - timebefore
+
+	if diff > (30 * 86400) {
+		return "超過1個月"
+	}
+
+	for i := 0; i < len(TimeUint); i++ {
+		if diff < TimeUint[i] {
+			continue
+		}
+		var temp = math.Floor(float64(diff / TimeUint[i]))
+		diff = diff % TimeUint[i]
+		if temp > 0 {
+			var tempStr string
+			tempStr = strconv.FormatFloat(temp, 'f', -1, 64)
+			strres = tempStr + StrUnit[i]
+		}
+		break
+	}
+	return strres
+}
+
 func HowLongAgoJp(timebefore int64, timenow int64) (strres string) {
 	var TimeUint = [6]int64{365 * 86400, 30 * 86400, 86400, 3600, 60, 1}
 	var StrUnit = [6]string{"年前", "ヶ月前", "日前", "時間前", "分前", "秒前"}
